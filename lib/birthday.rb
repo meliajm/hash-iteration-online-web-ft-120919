560a231e71e2fe696efdbcfc6f14e123bda78d3a
melia miller <meliajm@gmail.com>
2019-12-11 04:42:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,9 +14,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each {|name, age| 
     if age <= 12 
       puts "Happy Birthday #{name}! You are now #{age} years old!" 
-      
     else 
-      "You are too old for this." 
+      puts "You are too old for this." 
   end
   }
 end 
